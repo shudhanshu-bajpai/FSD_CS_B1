f72fe6532248dabe4ca2585621400a655d855104
Shudhanshu <shudhanshubajpai99@gmail.com>
2024-11-08 11:07:54 +0530

    new files are added

--- Test_CS_B_2200320120178/JS/api.js
+++ Test_CS_B_2200320120178/JS/api.js
@@ -1,6 +1,6 @@
 const weather = async () => {
     const city = document.getElementById("city").value;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},IN&appid=96e90262d1a0563740a9a1f7f601a74c&units=metric`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},IN&appid=bd5d6db1c55613f81c60eed85271139e&units=metric`)
     const data = await response.json()
     console.log(data)
     document.getElementById('cityname').innerText= data.name
